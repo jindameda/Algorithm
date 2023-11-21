b6c18cda2ddd40ba84d4e9af330a5d812c46338e
jindameda <107822509+jindameda@users.noreply.github.com>
2023-11-21 17:09:12 +0900

    [Bronze V] Title: 나머지, Time: 120 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split(" ");
+
+let A = parseInt(input[0]);
+let B = parseInt(input[1]);
+let C = parseInt(input[2]);
+
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
