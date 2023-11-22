29d5178ccab4ab48ae988af8da62ee75371b8958
jindameda <107822509+jindameda@users.noreply.github.com>
2023-11-22 15:27:51 +0900

    [Bronze V] Title: 사분면 고르기, Time: 116 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
@@ -1,18 +1,11 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
- const input = require('fs').readFileSync(0).toString().split('\n');
+const input = require('fs').readFileSync(0).toString().split('\n');
 
-const a = parseInt(input[0]);
-const b = parseInt(input[1]);
+const [x, y] = input;
 
-if (a > 0 && b > 0) {
-  console.log(1);
-} else if (a < 0 && b > 0) {
-  console.log(2);
-} else if (a < 0 && b < 0) {
-  console.log(3);
-} else if (a > 0 && b < 0) {
-  console.log(4);
+if (x > 0) {
+  console.log(y > 0 ? 1 : 4);
 } else {
-  console.log("Wrong Number");
+  console.log(y > 0 ? 2 : 3);
 }
