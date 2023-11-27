35719bcc7138adbd23dcf03ccb8e0996c4c262fb
jindameda <107822509+jindameda@users.noreply.github.com>
2023-11-27 10:47:36 +0900

    [Bronze V] Title: A+B - 7, Time: 156 ms, Memory: 9364 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+let answer = "";
+
+for (let i = 1; i <= input[0]; i++) {
+  const [a, b] = input[i].split(" ");
+  answer += `Case #${i}: ${parseInt(a) + parseInt(b) + "\n"}`;
+}
+
+console.log(answer);
