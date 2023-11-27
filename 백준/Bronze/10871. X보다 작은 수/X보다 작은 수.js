2bc05b0aea71f4396c9ff2183a06ada6d8e7e97e
jindameda <107822509+jindameda@users.noreply.github.com>
2023-11-27 14:41:44 +0900

    [Bronze V] Title: X보다 작은 수, Time: 124 ms, Memory: 10776 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
@@ -3,6 +3,6 @@ let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 const [n, x] = input[0].split(" ");
 const a = input[1].split(" ");
 
-const answer = a.filter((value) => Number(value) < Number(x));
+const answer = a.filter((value) => Number(value) < x);
 
 console.log(answer.join(" "));
