84507a4d6441c3d79744bf8fa83630458f14b251
jindameda <107822509+jindameda@users.noreply.github.com>
2023-12-04 16:00:52 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 132 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -0,0 +1,13 @@
+let input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("");
+
+let answer = [];
+
+for (let i = 97; i <= 122; i++) {
+  answer.push(input.indexOf(String.fromCharCode(i)));
+}
+
+console.log(answer.join(" "));
