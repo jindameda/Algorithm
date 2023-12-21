8aaf9c0e1d79ff3bba260224dd52980dbd4ec8c5
jindameda <107822509+jindameda@users.noreply.github.com>
2023-12-21 15:46:14 +0900

    [Bronze I] Title: 진법 변환 2, Time: 196 ms, Memory: 9380 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.js
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.js
@@ -1,6 +1,31 @@
-const fs = require("fs");
-let [num, divider] = fs.readFileSync("/dev/stdin").toString().trim().split(" ").map((item) => Number(item));
+const info = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
 
-let ans = num.toString(divider).toUpperCase();
+let n = Number(info[0]);
+const b = Number(info[1]);
+let result = [];
 
-console.log(ans);
+while (n >= b) {
+  let remainder = n % b;
+  let quotient = Math.trunc(n / b);
+
+  if (remainder >= 10) {
+    result.push(String.fromCharCode(remainder + 55));
+  } else {
+    result.push(remainder);
+  }
+
+  n = quotient;
+}
+
+if (n >= 10) {
+  result.push(String.fromCharCode(n + 55));
+} else {
+  result.push(n);
+}
+
+console.log(result.reverse().join(""));
