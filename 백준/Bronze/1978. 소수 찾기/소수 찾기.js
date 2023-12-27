d5400596fb9aea2666cc3b06b800b8fdb72baa93
jindameda <107822509+jindameda@users.noreply.github.com>
2023-12-27 12:40:45 +0900

    [Bronze II] Title: 소수 찾기, Time: 164 ms, Memory: 9696 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.js
@@ -0,0 +1,19 @@
+const input = require("fs")
+  .readFileSync(0)
+  .toString()
+  .trim()
+  .split("\n");
+
+const N = Number(input.shift());
+const array = input.shift().split(' ').slice(0, N).map(Number);
+
+const answer = array.filter((number) => {
+  for (let i = 2; number > i; i+=1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return number > 1;
+}).length;
+
+console.log(answer);
