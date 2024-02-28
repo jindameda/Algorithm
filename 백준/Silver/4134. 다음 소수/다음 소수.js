0c115e5b9a3cf04a3bf02168e8531494c3ece9fa
jindameda <107822509+jindameda@users.noreply.github.com>
2024-02-28 16:41:30 +0900

    [Silver IV] Title: 다음 소수, Time: 384 ms, Memory: 11272 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.js
@@ -0,0 +1,30 @@
+let input = require("fs")
+  .readFileSync(0)
+  .toString()
+  .trim()
+  .split("\n");
+
+input.shift();
+let arr = input.map(Number);
+let answer = [];
+
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+
+while (arr.length != 0) {
+  if (isPrime(arr[0])) {
+    answer.push(arr[0]);
+    arr.shift();
+  } else {
+    arr[0]++;
+  }
+}
+
+console.log(answer.join("\n"));
