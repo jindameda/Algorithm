1b7e74a3c670dd093fa86da3113d3dc9b20b26c5
jindameda <107822509+jindameda@users.noreply.github.com>
2024-02-29 14:28:47 +0900

    [Silver III] Title: 소수 구하기, Time: 320 ms, Memory: 50508 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.js
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.js
@@ -5,14 +5,19 @@ const [M, N] = require("fs")
   .split(" ")
   .map(Number);
 
-const isPrime = (num) => {
-  if (num === 1) return;
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) return;
+const prime = { 1: true };
+
+for (let i = 2; i <= Math.sqrt(N); i++) {
+  if (prime[i]) continue;
+  for (let j = i ** 2; j <= N; j += i) {
+    prime[j] = true;
   }
-  return console.log(num);
-};
+}
+
+const results = [];
 
 for (let i = M; i <= N; i++) {
-  isPrime(i);
+  if (!prime[i]) results.push(i);
 }
+
+console.log(results.join("\n"));
